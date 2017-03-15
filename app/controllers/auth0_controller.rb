5b1546c03e95ba87f687a2ae487eed0cfc207de7
Luis Carlos Zapparolli Neto <lczapparolli@gmail.com>
2017-03-15 01:38:07 -0300

    Added rescue clause to Auth0 callback method
    Block added to debug logon and login error in production

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -3,17 +3,21 @@ class Auth0Controller < ApplicationController
      # example request.env['omniauth.auth'] in https://github.com/auth0/omniauth-auth0#auth-hash
     # id_token = session[:userinfo]['credentials']['id_token']
     # store the user profile in session and redirect to root
-    session[:code] = request.params['code']
-    session[:userinfo] = request.env['omniauth.auth']
+    begin
+      session[:code] = request.params['code']
+      session[:userinfo] = request.env['omniauth.auth']
 
-    user = User.find_by(auth_token: session[:userinfo]["uid"])
-    unless user
-      user = User.new
-      user.auth_token = session[:userinfo]["uid"]
-      user.save
+      user = User.find_by(auth_token: session[:userinfo]["uid"])
+      unless user
+        user = User.new
+        user.auth_token = session[:userinfo]["uid"]
+        user.save
+      end
+
+      redirect_to :root
+    rescue => ex
+      logger.error ex.message
     end
-
-    redirect_to :root
   end
 
   def failure
