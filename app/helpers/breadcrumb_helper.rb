44c03f1c93be7e5812df2db187c22507fc221125
Luis Carlos Zapparolli Neto <lczapparolli@gmail.com>
2018-06-14 22:32:17 -0300

    Improving breadcrumbs helper
    - Using rails helper methods to generate html tags

--- app/helpers/breadcrumb_helper.rb
+++ app/helpers/breadcrumb_helper.rb
@@ -3,33 +3,35 @@ module BreadcrumbHelper
   def breadcrumb
     #if is home page, we do not need breadcrumbs
     unless current_page? root_path
-      html = "<ul class=\"breadcrumbs\">"
-
-      html << "<li>" << link_to("Home", root_path, class: "link link-default") << "</li>"
-
-      #if is de index action of any controller, only show as current item
-      if action_name == "index"
-        html << "<li class=\"current\" >" << controller.controller_name.capitalize << "</li>"
-      elsif controller.respond_to?(:index) #if contains a 'index' method treat like a resource controller
-        html << "<li>" << link_to(controller.controller_name.capitalize, controller: controller.controller_name, action: "index") << "</li>"
-
-        if action_name == "edit" #when the action is edit, link to show item
-          html << "<li>" << link_to(controller.resource_description, controller: controller.controller_name, action: "show") << "</li>"
-          html << "<li class=\"current\" >Edit</li>"
-        elsif action_name == "show"
-          html << "<li class=\"current\" >" << controller.resource_description << "</li>"
-        elsif action_name == "new"
-          html << "<li class=\"current\" >New</li>"
-        end
-
-      else #if does not contain a 'index' method, it is not a resource controller, just show the action
-        html << "<li class=\"current\" >" << action_name.tr("_", " ").capitalize << "</li>"
+      content_tag(:div, class: "breadcrumbs") do
+        link_to("Home", root_path, class: "link link-default") <<
+        controller_tag() <<
+        current_action_tag()
       end
-
-      html << "</ul>"
-      html.html_safe
     end
-    #controller.controller_name
+
+  end
+
+  def controller_tag
+    #if is de index action of any controller, only show as current item
+    if action_name == "index"
+      sanitize(" > ") << content_tag(:span, controller.controller_name.capitalize, class: "current")
+    elsif controller.respond_to?(:index) #if contains a 'index' method treat like a resource controller
+      sanitize(" > ") << link_to(controller.controller_name.capitalize, controller: controller.controller_name, action: "index")
+    else
+      sanitize(" > ") << content_tag(:span, action_name.tr("_", " ").capitalize, class: "current")
+    end
+  end
+
+  def current_action_tag
+    if action_name == "edit" #when the action is edit, link to show item
+      sanitize(" > ") << link_to(controller.resource_description, controller: controller.controller_name, action: "show") <<
+      sanitize(" > ") << content_tag(:span, "Edit", class: "current")
+    elsif action_name == "show"
+      sanitize(" > ") << content_tag(:span, controller.resource_description, class: "current")
+    elsif action_name == "new"
+      sanitize(" > ") << content_tag(:span, "New", class: "current")
+    end
   end
 
 end
